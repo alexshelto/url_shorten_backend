2a876df82e01d095f5f5a5452f81500a2318c316
Alex Shelton <39957709+alexshelto@users.noreply.github.com>
2023-06-08 10:43:37 -0500

    Change os vars

--- Models/setup.go
+++ Models/setup.go
@@ -2,7 +2,6 @@ package models
 
 import (
     "log"
-    "github.com/joho/godotenv"
     "gorm.io/driver/mysql"
     "gorm.io/gorm"
     "os"
@@ -13,12 +12,11 @@ var DB *gorm.DB
 
 func ConnectDatabase() {
 
-    err := godotenv.Load(".env")
-    if err != nil {
-        log.Fatalf("Some error occured. Err: %s", err)
-    }
-
     db_connection_string := os.Getenv("MYSQL_DSN")
+
+    if db_connection_string == "" {
+        log.Fatalf("No db string")
+    }
 
     db, err := gorm.Open(mysql.Open(db_connection_string), &gorm.Config{})
     if err != nil {
